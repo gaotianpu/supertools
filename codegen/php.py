d093587e355a0c3b1d6cecc1d677e7d0ea25a588
tgao <gaotianpu@gmail.com>
2015-10-12 22:05:09 +0800

    gen php code
    auto gencode

---
+++ codegen/php.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import web
+
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+
+# 基于如下设计：
+#http://www.ruanyifeng.com/blog/2010/12/php_best_practices.html
+#
+source_dir = '/Users/gaotianpu/github/wdc/plants/php/'
+
+dbr = web.database(dbn='mysql', host='192.168.1.111', db='plants', user='root', pw='root')
+
+def load_tables():    
+    return [t.Tables_in_plants for t in list(dbr.query('show tables'))]
+    
+
+def load_fields(table_name):
+    return list(dbr.query('show columns from %s'%(table_name)))
+    # r.Field,r.Type,r.Null,r.Key,r.Default,r.Extra 
+
+def save_file(lfile,li):
+    if os.path.exists(lfile): 
+        os.system('cp -f %s %s.bak' % (lfile,lfile) ) 
+        os.system('rm -f %s' % (lfile) )
+
+    print '\r\n'.join(li)+'\r\n'
+    with open(lfile,'w') as f:
+        f.write('\r\n'.join(li)+'\r\n')
+        f.close()
+
+formatTableName = lambda table_name: ''.join([x.capitalize() for x in table_name.split('_')]) 
+
+def gen_model(dir):
+    tables = load_tables()
+    for t in tables:
+        className = formatTableName(t) 
+        fields = load_fields(t) 
+
+        li = []
+        li.append('class %s {' % (className) )
+        li.append('var %s;' % (','.join('$%s' % (f.Field)  for f in fields)  ) ) #$id, $first_name, $last_name, $email
+        li.append('}')
+
+        lfile = '%s%s.inc' % (dir,className)
+        save_file(lfile,li)
+
+        
+def gen_dao(dir):
+    tables = load_tables()
+    for t in tables:
+        className = '%sDAO' % (formatTableName(t) )
+        fields = load_fields(t)
+
+        li = []
+        li.append('class %s {' % (className))
+
+        li.append('var $conn;')
+        li.append('function %s(& $conn) {$this->conn =&$conn;}' % (className) )
+
+        li.append('function save(&$vo) { if ($v->pk_id == 0) { $this->insert($vo); } else { $this->update($vo); } }')
+
+        li.append('function get($pk_id) {')        
+        li.append('　　}')
+
+        li.append('　　function delete(&$vo) {')
+        li.append('　　}') 
+
+        li.append('#-- private functions')
+        li.append('　function getFromResult(&vo, $result) {')
+        li.append('　}') 
+
+        li.append('　function update(&$vo) {')
+        li.append('　　　}')
+
+        li.append('　　function insert(&$vo) {')
+        li.append('　}')
+
+        li.append('}')
+
+        lfile = '%s%s.inc' % (dir,className)
+        save_file(lfile,li)
+
+
+def run():    
+    lib_dir = '%slib/'%(source_dir)
+    common_lib_dir = '%slib/common/'%(source_dir)
+    model_lib_dir = '%slib/model/'%(source_dir)
+    dao_lib_dir = '%slib/dao/'%(source_dir)
+    logic_lib_dir = '%slib/logic/'%(source_dir)
+    parts_dir = '%sparts/'%(source_dir)
+    control_dir = '%scontrol/'%(source_dir)
+
+    os.system('mkdir %s'%(source_dir))
+    os.system('mkdir %s' % (lib_dir))
+    os.system('mkdir %s' % (common_lib_dir))
+    os.system('mkdir %s' % (model_lib_dir))
+    os.system('mkdir %s' % (dao_lib_dir))
+    os.system('mkdir %s' % (logic_lib_dir))
+    os.system('mkdir %s' % (parts_dir)   )  
+    os.system('mkdir %s' % (control_dir))
+    
+    gen_model(model_lib_dir) 
+    gen_dao(dao_lib_dir) 
+
+
+if __name__ == '__main__':
+    run()
+
+
+
+
+
+    
